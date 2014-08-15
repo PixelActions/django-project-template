378f9a3e1df4ea54736b836751c5966f3adab916
Kyriakos Toumbas <kyriakos@toumbas.com.cy>
2014-08-15 20:43:50 +0300

    updated default settings for commonly used apps

--- project_name/project_name/settings/base.py
+++ project_name/project_name/settings/base.py
@@ -1,12 +1,5 @@
-"""
-Django settings for {{project_name}} project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS, TEMPLATE_LOADERS, STATICFILES_FINDERS
+from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
@@ -17,7 +10,7 @@ PROJECT_ROOT = os.path.join(BASE_DIR, '..')
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '{{SECRET_KEY}}'
+SECRET_KEY = '{{ SECRET_KEY }}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,12 +23,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    #'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'south',
+    #'apps.pages',
+    #'crispy_forms',
+    #'easy_thumbnails',
+    #'rosetta',
+    
 )
 
 MIDDLEWARE_CLASSES = (
@@ -89,3 +89,60 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+
+#FOR MULTILINGUAL WEBSITES:
+#   ENABLE THE LocalMiddelware EXACTLY after SessionMiddleware
+#   Setup the project languages
+'''
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('el', _('Greek')),
+)
+
+#You can enable the i18n context processor and use the provided view to change between languages (or use different links with en/el values before the rest of the link
+TEMPLATE_CONTEXT_PROCESSORS += (
+    'django.core.context_processors.request',
+    'django.core.context_processors.i18n',
+)
+#Lastly, point to the LOCALE folder in your project, that the translations should be stored.
+#Run ./manage.py makemessage -l <language code> 
+#for each language, to generate the language file. later, with compilemessages, you can install the translations you used.
+LOCALE_PATHS = (
+    os.path.join(PROJECT_ROOT, 'locale'),
+)
+'''
+
+
+
+#GRAPPELI SETTINGS
+#GRAPPELLI_ADMIN_TITLE = "{{project_name}} Administration"
+
+#CRISPY FORMS SETTINGS
+#CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+
+#EASY_THUMBNAILS SETTINGS
+'''
+SOUTH_MIGRATION_MODULES = {
+    'easy_thumbnails': 'easy_thumbnails.south_migrations',
+}
+THUMBNAIL_SUBDIR = 'thumbs'
+THUMBNAIL_ALIASES = {
+    '': {
+        'example': {'size': (400,0), 'crop':'scale'},
+        'example2': {'size': (400,0), 'upscale': True, 'crop':'smart'},
+    },
+}
+
+'''
--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,8 +1,34 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+
 admin.autodiscover()
 
+
+#If HomeView from pages app is gonna be used, uncomment this and the appropriate url entry
+#from apps.pages.views import HomeView
+
 urlpatterns = patterns('',
+    #Enable if grappelli is installed
+    #url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
+    #url(r'^pages/', include('apps.pages.urls')),
+    #url(r'^$', HomeView.as_view(), name='home'),
 )
+
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$',
+            'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, }),
+    )
+    
+
+urlpatterns += staticfiles_urlpatterns()
+
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('',
+        url(r'^rosetta/', include('rosetta.urls')),
+    )
