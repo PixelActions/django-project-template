08c6875c4b1e34cc37d8ca718b125a7f1aaa74a8
Kyriakos Toumbas <kyriakos@pixelactions.com>
2016-07-15 12:04:24 +0300

    removed unecessary / from url conf

--- project_name/pa_apps/pages/urls.py
+++ project_name/pa_apps/pages/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^(?P<page_template_name>[\w-]+)/$', views.AreasDetailAutoFind.as_view(), name='template_locator'),
-    #url(r'^/subsection/(?P<page_template_name>[\w-]+)/$', views.AreasDetailAutoFind.as_view(subfolder='subsection'), name='template_locator_subsection'),
+    #url(r'^subsection/(?P<page_template_name>[\w-]+)/$', views.AreasDetailAutoFind.as_view(subfolder='subsection'), name='template_locator_subsection'),
     url(r'^contact/$', views.ContactView.as_view(), name='pages_contact'),
 )
