19e037b9ef8ed746f328535f052f29189d385e85
Kyriakos Toumbas <kyriakos@pixelactions.com>
2016-10-07 14:27:54 +0300

    updates pages.urls for working on django 1.10

--- project_name/pa_apps/pages/urls.py
+++ project_name/pa_apps/pages/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^(?P<page_template_name>[\w-]+)/$', views.TemplateLocatorView.as_view(), name='template_locator'),
     #url(r'^subsection/(?P<page_template_name>[\w-]+)/$', views.TemplateLocatorView.as_view(subfolder='subsection'), name='template_locator_subsection'),
     #url(r'^contact/$', views.ContactView.as_view(), name='pages_contact'),
-)
+]
